c05cfc5213e7f4d1fa7b64b1a0cbede8fb248a37
nemanjam <nemanja.mitic.elfak@hotmail.com>
2020-04-11 01:08:05 +0200

    change github button

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,16 +7,13 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-content">
         <span className="username">@nemanjam 2020</span>
-        {/* Place this tag where you want the button to render. */}
-        <a
-          className="github-button"
-          href="https://github.com/nemanjam/mern-boilerplate"
-          data-icon="octicon-star"
-          data-size="large"
-          aria-label="Star nemanjam/mern-boilerplate on GitHub"
-        >
-          Star
-        </a>
+        <iframe
+          src="https://ghbtns.com/github-btn.html?user=nemanjam&repo=mern-boilerplate&type=star&count=true&size=large"
+          frameborder="0"
+          scrolling="0"
+          width="160px"
+          height="30px"
+        ></iframe>
       </div>
     </div>
   );
